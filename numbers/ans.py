39658cb8f8253e30d4e322079a1d5c793c057122
Devraj Mehta <devm33@gmail.com>
2016-05-06 12:22:25 -0400

    bugfix, all tests passing now

--- numbers/ans.py
+++ numbers/ans.py
@@ -18,11 +18,6 @@ def printnum(num):
         ans.append(ORDER[order])
         order += 1
 
-        cur_hundred = num % 1000
-        if cur_hundred > 100:
-            ans.append('hundred')
-            ans.append(DIGIT[cur_hundred // 100])
-
         cur_ten = num % 100
         if 9 < cur_ten < 20:
             ans.append(TEEN[cur_ten - 10])
@@ -31,6 +26,11 @@ def printnum(num):
             if cur_digit > 0:
                 ans.append(DIGIT[cur_digit])
             ans.append(TEN[cur_ten // 10])
+
+        cur_hundred = num % 1000
+        if cur_hundred > 100:
+            ans.append('hundred')
+            ans.append(DIGIT[cur_hundred // 100])
 
         num = num // 1000
 
