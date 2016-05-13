124d14752d5135884a11de23ea90c06114ad897c
Devraj Mehta <devm33@gmail.com>
2016-05-13 14:27:57 -0400

    maybe cleaner, not faster though :(

--- stringchains/ans.py
+++ stringchains/ans.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import unittest
+
 def offByOne(a, b):
     """return true if b can be made by removing one character from a.
     assumes len(a) == len(b) + 1
@@ -22,6 +24,18 @@ def offByOne(a, b):
     return True
 
 
+class TestOffByOne(unittest.TestCase):
+
+    def testPrefix(self):
+        self.assertTrue(offByOne("abcd", "abc"))
+
+    def testInfix(self):
+        self.assertTrue(offByOne("abcd", "acd"))
+
+    def testFalse(self):
+        self.assertFalse(offByOne("abcd", "acb"))
+
+
 def longestChain(words):
     word_len = {}
     for word in words:
@@ -31,37 +45,28 @@ def longestChain(words):
         else:
             word_len[l] = [{'word': word, 'chain': 1}]
 
-    lens = word_len.keys()
-    max_limit = 1
-    cur_chain = 1
-    prev = -2
-    for l in sorted(lens):
-        if l == prev + 1:
-            cur_chain += 1
-            if cur_chain > max_limit:
-                max_limit = cur_chain
-        else:
-            cur_chain = 1
-        prev = l
-    cur = prev
-    while cur > 0:
-        if cur in word_len and cur - 1 in word_len:
-            for word_dict in word_len[cur - 1]:
-                matches = [wd for wd in word_len[cur] if offByOne(wd['word'], word_dict['word'])]
+    max_chain = 1
+    lengths = list(reversed(sorted(word_len.keys())))
+    for i in range(1, len(lengths)):
+        prev = lengths[i -1]
+        cur = lengths[i]
+        if prev == 1 + cur:
+            for word_dict in word_len[cur]:
+                matches = [wd for wd in word_len[prev] if offByOne(wd['word'], word_dict['word'])]
                 if matches:
                     word_dict['chain'] += max([wd['chain'] for wd in matches])
-                    if word_dict['chain'] == max_limit:
-                        return max_limit
-        cur -= 1
-    return max([wd['chain'] for wds in word_len.values() for wd in wds])
+                    if word_dict['chain'] > max_chain:
+                        max_chain = word_dict['chain']
+    return max_chain
 
-import unittest
 
 class TestStringChain(unittest.TestCase):
 
-  def testStringChain(self):
-     self.assertEqual(4, longestChain(['a', 'b', 'ba', 'bca', 'bda', 'bdca']))
+    def testStringChain(self):
+        self.assertEqual(4, longestChain(['a', 'b', 'ba', 'bca', 'bda', 'bdca']))
 
+    def testNoChain(self):
+        self.assertEqual(1, longestChain(['a', 'b', 'cd', 'efg', 'ghi', 'abcg']))
 
 
 if __name__ == '__main__':
